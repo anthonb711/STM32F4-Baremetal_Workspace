4f070c4d8f9b0023ffe81b5ad8fbe70590ae6411
anthonb711 <anthonb711@gmail.com>
2025-09-08 21:56:45 -0400

    uart-DMA iniit steps partial

---
+++ 20_uart_tx_dma/Inc/uart.h
@@ -0,0 +1,20 @@
+
+
+#ifndef UART_H_
+#define UART_H_
+
+#include <stdint.h>
+#include "stm32f411xe.h"
+
+#define SR_RXNE			(1U<<5)		// RX not empty, only try to read if data is present
+
+void dma1_stream6_init          (uint32_t src, uint32_t dst, uint32_t len	);
+char uart2_read					(void  										);
+void uart2_tx_init				(void										);
+void uart2_rx_interrupt_init	(void										);
+void uart2_write				(int ch										);
+
+
+
+
+#endif /* UART_H_ */
