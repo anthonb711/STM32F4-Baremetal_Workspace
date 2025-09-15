b32c13a02c1949bd14fcd1a9569a4b9d36144a9e
anthonb711 <anthonb711@gmail.com>
2025-09-15 16:02:59 -0400

    GPIO configured

--- 21_I2C_l3gd20/Src/i2c.c
+++ 21_I2C_l3gd20/Src/i2c.c
@@ -7,8 +7,12 @@
 #include "stm32f411xe.h"
 #include "i2c.h"
 
-#define I2C1EN 	(1U<<21)
-#define GPIOB	(1U<<1)
+#define I2C1EN 			(1U<<21)
+#define GPIOBEN			(1U<<1)
+#define	OTYPER_OT8		(1U<<8)
+#define OTYPER_OT9		(1U<<9)
+#define CR2_FREQ		(1U<<4)	// 16Mhz
+
 
 
 
@@ -19,15 +23,53 @@ void main(void){
 
 void I2C1_init(void)
 {
+
+	/* enable clock access to GPIOB*/
+	RCC->AHB1ENR |= GPIOBEN;
+
+	/* set PB8 & PB9 mode to alt func */
+	GPIOB->MODER &=~(1U<<16);
+	GPIOB->MODER |= (1U<<17);
+
+	GPIOB->MODER &=~(1U<<18);
+	GPIOB->MODER |= (1U<<19);
+
+	/* PB8 SCL - alt function I2C1 */
+	GPIOB->AFR[1] |= (1U<<0);
+	GPIOB->AFR[1] &=~(1U<<1);
+	GPIOB->AFR[1] |= (1U<<2);
+	GPIOB->AFR[1] &=~(1U<<3);
+
+	/* PB9 SDA - alt function I2C1*/
+	GPIOB->AFR[1] |= (1U<<4);
+	GPIOB->AFR[1] &=~(1U<<5);
+	GPIOB->AFR[1] |= (1U<<6);
+	GPIOB->AFR[1] &=~(1U<<7);
+
+	/* set PB8 & PB9 output type to open drain */
+	GPIOB->OTYPER |= OTYPER_OT8;
+	GPIOB->OTYPER |= OTYPER_OT9;
+
+	/* enable the pullup for PB8 & PB9*/
+	GPIOB->PUPDR |= (1U<<16);
+	GPIOB->PUPDR &=~(1U<<17);
+
+	GPIOB->PUPDR |= (1U<<18);
+	GPIOB->PUPDR &=~(1U<<19);
+
 	/* enable clock access to I2C1*/
 	RCC->APB1ENR |= I2C1EN;
 
-	/* enable clock access to GPIOB*/
-	RCC->AHB1ENR |= GPIOB;
 
+	/* enter reset mode */
+	I2C1->CR1 |= (1U<<15);
 
-	/* set PB8 & PB9 mode to alt func */
-	/* set PB8 & PB9 output type to open drain */
-	/* enable the pullup for PB8 & PB9*/
+	/* come out of reste*/
+	I2C1->CR1 &=~(1U<<15);
+
+	/* configure defualt clock*/
+	I2C1->CR2 |= CR2_FREQ;
+
+	/* set I2C clock mode in the CCR */
 }
 
