74a0879c4d73ff23ee01d8a44f19c98005b2979f
anthonb711 <anthonb711@gmail.com>
2025-09-18 06:43:19 -0400

    l3gd20 register defines

--- 21_I2C_l3gd20/Inc/l3gd20.h
+++ 21_I2C_l3gd20/Inc/l3gd20.h
@@ -4,27 +4,47 @@
 #include "i2c.h"
 
 
-/*****************************************************
- * L3GD20 Adafruit Triple Axis Gyro break-out board
- * SUPPLY VOLTAGE:	3.3v - 5V
- * PINS:
- *	SCL -> PB8
- *	SDA -> PB9
- *	SA0
- *	CS
- *	DRDY
- *	INT1
- *	GND -> GND
- *	3Vo -> +3
- *	Vin
- *
- *	Data needed from Datasheet
- *	Register Map:
- *		* Device ID register	-->	WHO_AM_I
- *		* Slave Addr (SAD)		-->	0b110101 (0x35)
- ****************************************************/
+/*****************************************************************
+  	  ~~~ L3GD20 Adafruit Triple Axis Gyro break-out board ~~~
 
-#define WHO_AM_I_R	0x0F //  *WHO_AM_I = 0b1101 0100
+ SUPPLY VOLTAGE:	3.3v - 5V
+ PINS:
+ 	 * SCL -> PB8
+ 	 * SDA -> PB9
+ 	 * SA0
+ 	 * CS
+ 	 * DRDY
+ 	 * INT1
+	 * GND -> GND
+	 * 3Vo -> +3
+	 * Vin
+
+ DEVICE ID REGISTER:		WHO_AM_I  (0x0F)
+ SLAVE ADDR (SAD):			0b110100  (0x34); if SDO pin is high 0b110101 (0x35)
+ POWER CONTROL:				CTRL_REG1 (0x20)
+ DATA FORMAT:				CTRL_REG4 (0x23)
+  *-------------------------------------------------------------*
+  |			*** SAD+READ/WRITE PATTERNS ***                    	|
+  |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+  | COMAMND	| SAD[6:1]	| SAD[0] = SDO	|  R/W	| 	SAD+R/W		|
+  |---------|-----------|---------------|-------|---------------|
+  | READ 	| 110101	| 		0		|	1	| 11010101(D5)	|
+  |---------|-----------|---------------|-------|---------------|
+  | WRITE	| 110101	|		0		|	0	| 11010100 D4)	|
+  |---------|-----------|---------------|-------|---------------|
+  | READ	| 110101	|		1		|	1	| 11010111(D7)	|
+  |---------------------|---------------|-------|---------------|
+  | WRITE	| 110101	|		1		|	0	| 11010110(D6)	|
+  *---------*-----------*---------------*-------*---------------*
+
+
+
+ ****************************************************************/
+
+#define WHO_AM_I_R	(0x0F)	// *WHO_AM_I = 0b1101 0100
+#define DEVICE_ADDR (0x35)	// SDO is connected to GND
+#define CTRL_REG4_R	(0x23)	// used for data formating
+#define CTRL_REG1_R	(0x20)	// used for power control
 
 
 
