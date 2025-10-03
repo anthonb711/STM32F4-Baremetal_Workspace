58f0a9b867239e9796b2dd7fb338176deac16163
Anthon Brown && <bro13111@byui.edu>
2025-10-03 16:03:59 -0400

    23_ gpio init complete

--- 23_lcd_2004/Inc/lcd_2004.h
+++ 23_lcd_2004/Inc/lcd_2004.h
@@ -4,11 +4,41 @@
 #include "stm32f411xe.h"
 #include"i2c.h"
 
-#define SLAVE_ADDR 		(0x27)
+/* GIPO defines */
+#define GPIOAEN	(1U<<0)
+#define GPIOCEN	(1U<<2)
+#define PA3		(1U<<3)
+#define PA5		(1U<<5)
+#define	PA6		(1U<<6)
+#define PC0		(1U<<0)
+#define PC1		(1U<<1)
+#define PC2		(1U<<2)
+#define PC3		(1U<<3)
+#define PC4		(1U<<4)
+#define PC5		(1U<<5)
+#define PC6		(1U<<6)
+#define PC7		(1U<<7)
 
+/* 2004 defines */
+#define E_PIN	PA3
+#define RS_PIN	PA5
+#define RW_PIN	PA6
+#define D0_PIN	PC0
+#define D1_PIN	PC1
+#define D2_PIN	PC2
+#define D3_PIN	PC3
+#define D4_PIN	PC4
+#define D5_PIN	PC5
+#define D6_PIN	PC6
+#define D7_PIN	PC7
 
-void lcd_init 	(void);
-void gpio_init	(void);
+/* 2004 cmd defines */
+#define
+
+void lcd_init 	(void				);
+void gpio_init	(void				);
+void lcd_cmd	(unsigned char cmd	);
+void lcd_data	(char data			);
 
 
 
--- 23_lcd_2004/Src/lcd_2004.c
+++ 23_lcd_2004/Src/lcd_2004.c
@@ -2,13 +2,89 @@
 #include "i2c.h"
 
 
+void gpio_init	(void)
+{
+	/* enable clock access for GPIOA and GPIOC */
+	RCC->AHB1ENR |= (GPIOAEN | GPIOCEN);
+
+	/* set GPIO directions -  keep it friendly */
+	/* PA3 E_PIN */
+	GPIOA->MODER |= (1U<<6);
+	GPIOA->MODER &=~(1U<<7);
+	/* PA5 RS_PIN */
+	GPIOA->MODER |= (1U<<10);
+	GPIOA->MODER &=~(1U<<11);
+	/* PA6 RW_PIN */
+	GPIOA->MODER |= (1U<<12);
+	GPIOA->MODER &=~(1U<<13);
+
+	/* PC0 D0_PIN */
+	GPIOC->MODER |= (1U<<0);
+	GPIOC->MODER &=~(1U<<1);
+	/* PC1 D1_PIN */
+	GPIOC->MODER |= (1U<<2);
+	GPIOC->MODER &=~(1U<<3);
+	/* PC2 D2_PIN */
+	GPIOC->MODER |= (1U<<4);
+	GPIOC->MODER &=~(1U<<5);
+	/* PC3 D3_PIN */
+	GPIOC->MODER |= (1U<<6);
+	GPIOC->MODER &=~(1U<<7);
+	/* PC4 D4_PIN */
+	GPIOC->MODER |= (1U<<8);
+	GPIOC->MODER &=~(1U<<9);
+	/* PC5 D5_PIN */
+	GPIOC->MODER |= (1U<<10);
+	GPIOC->MODER &=~(1U<<11);
+	/* PC6 D6_PIN */
+	GPIOC->MODER |= (1U<<12);
+	GPIOC->MODER &=~(1U<<13);
+	/* PC7 D7_PIN */
+	GPIOC->MODER |= (1U<<14);
+	GPIOC->MODER &=~(1U<<15);
+
+	/* E & RW need to init LOW */
+	GPIOA->BSRR |= (1U<<19);
+	GPIOA->BSRR |= (1U<<22);
+}
+
 
 void lcd_init(void)
 {
-	i2c_init();
-
+	/*  */
+	/*  */
 
 }
+
+
+void lcd_cmd(unsigned char cmd)
+{
+	/* set the RS and RW LOW for IR write */
+	GPIOA->BSRR = (RS_PIN | RW_PIN) << 16;
+
+	/* output the command */
+	GPIOC->ODR = cmd;
+
+	/* HIGH LOW Pulse on E pin */
+	GPIOA->BSRR |= E_PIN;
+	GPIOA->BSRR |= E_PIN <<16;
+}
+
+
+void lcd_data(char data)
+{
+	/*  */
+	/*  */
+}
+
+
+
+
+
+
+
+
+
 
 
 
